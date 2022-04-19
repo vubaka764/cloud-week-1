1794bdda99d824ab70395e6a45c53c3fb64134f5
Vitalis ogunwa <ogunwav@yahoo.com>
2022-04-19 02:02:01 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+//first NodeJS application 
+ 
+const express = require('express') 
+const app = express() 
+const port = 3000 
+ 
+app.get('/', (req, res) => { 
+  res.send('Hello Virtual Machine!') 
+}) 
+ 
+app.listen(port, () => { 
+  console.log(`Express Application  listening at port 3000`) 
+}) 
+ 
